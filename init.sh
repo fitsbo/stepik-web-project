277a38a1aab50f3cbea417704e182e9af211202f
Mikhail Gnedoy <gnedoy85@gmail.com>
2018-07-12 01:14:08 +0700

    Add: server configs for django project

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/hello.conf   /etc/gunicorn.d/hello
+sudo ln -s /home/box/web/etc/django.conf   /etc/gunicorn.d/django
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
