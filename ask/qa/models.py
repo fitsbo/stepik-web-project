3955c33de731e006db217d43f4e7ccb57e74fb4a
Mikhail Gnedoy <gnedoy85@gmail.com>
2018-07-15 02:24:38 +0700

    Add Question and Answe models, Question manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    added_at = models.DateTimeField(
+        blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='qa_user_likes')
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/question/{}/'.format(self.id)
+
+
+class Answer(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    added_at = models.DateTimeField(
+        blank=True, auto_now_add=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
